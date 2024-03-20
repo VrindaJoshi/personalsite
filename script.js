62cdbc48807cdc33148716ea2550313dd1268234
vrindaj <vrinda.joshi@hotmail.com>
2024-03-19 22:25:55 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+/*
+function Comment(props) {
+  return (
+    <div className="container projects">
+      <div className="row row-cols-2">
+        <div className="col-lg-4 col-md-6 col-12">
+          <img src={props.image} className="img-fluid"></img>
+        </div>
+        <div className="col-lg-8 col-md-6 col-12 project-text">
+          <h3 className="project-title">{props.title}</h3>
+          <p>{props.text}</p>
+        </div>
+        <a className="project-link" href={props.link}>see the code ↗</a>
+      </div>
+    </div>
+  );
+}
+
+const project1 = {
+  image:"images/placeholder-3.webp",
+  title:"PROJECT 1",
+  text:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Cras tincidunt nunc massa, sit amet semper purus mattis a.",
+  link:"#"
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Comment
+    image={project1.image}
+    title={project1.title}
+    text={project1.text} 
+    link={project1.link} />
+
+);
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {style:{color:"red"}, id: 'someId', className: "row row-cols-2"},
+
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+*/
+
 // CODE FROM https://css-tricks.com/snippets/css/typewriter-effect/
 document.addEventListener('DOMContentLoaded',function(event){
         
@@ -23,4 +69,4 @@ document.addEventListener('DOMContentLoaded',function(event){
     setTimeout(() => { typeWriter("scroll down 4 more!", 0, "h4", function(){
     }) }, 4000);
     
-  });  
+  }); 
